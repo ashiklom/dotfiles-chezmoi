d9beda153a003fb7c2486de96a614c7d4de55c06
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2026-09-18 09:23:10 -0400

    better send selection

--- dot_config/nvim/lua/ans-utils.lua
+++ dot_config/nvim/lua/ans-utils.lua
@@ -1,13 +1,12 @@
 local M = {}
 
 function M.get_visual_selection()
-  local region = vim.fn.getregionpos(vim.fn.getpos("."), vim.fn.getpos("v"))[1]
-  local bufnum = region[1][1]
-  local start_line = region[1][2] - 1
-  local start_col = region[1][3] - 1
-  local end_line = region[#region][2] - 1
-  local end_col = region[#region][3]
-  local lines = vim.api.nvim_buf_get_text(bufnum, start_line, start_col, end_line, end_col, {})
+  local mode = vim.fn.mode()
+  if mode ~= "v" and mode ~= "V" and mode ~= "\22" then
+    -- not called from visual mode; fall back to the last selection
+    mode = vim.fn.visualmode()
+  end
+  local lines = vim.fn.getregion(vim.fn.getpos("."), vim.fn.getpos("v"), { type = mode })
   return table.concat(lines, "\n")
 end
 
