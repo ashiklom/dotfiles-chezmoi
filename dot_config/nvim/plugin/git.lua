0989aae937f99072286154a344acad5c7c0c3a7f
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2026-09-25 14:07:59 -0400

    remove lazygit plugin (use snacks instead)

--- dot_config/nvim/plugin/git.lua
+++ dot_config/nvim/plugin/git.lua
@@ -32,9 +32,3 @@ require('diffview').setup({
     }
   }
 })
-
-vim.pack.add({
-  "https://github.com/kdheepak/lazygit.nvim",
-  "https://github.com/nvim-lua/plenary.nvim"
-})
-vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<CR>", { desc = "Lazygit" })
